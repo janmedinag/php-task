a083941adbd4be22ca68b4b86437bf997ba5dea7
janmedinag <janmedinag@gmail.com>
2023-06-18 19:24:39 +1000

    janmedinag: Add file and dry run command

--- user_upload.php
+++ user_upload.php
@@ -30,6 +30,60 @@
                 echo "MYSQL host: ".$hostname;
                 break;                
             case isset($options["file"]):
+                $stdouts = fopen('stdout.log', 'r');
+                $line = '';
+
+                while (!feof($stdouts)) {
+                    $line = $line.fgets($stdouts);
+                }
+
+                fclose($stdouts);
+
+                $stdout = fopen('stdout.log', 'w');
+
+                $conn = new mysqli($hostname, $username, $password, $dbname);
+                if ($conn->connect_error) {
+                    fwrite($stdout, $line.date("d/m/Y H:i:s")." Database Connection failed: ". $conn->connect_error."\n");
+                    echo date("d/m/Y H:i:s")." Database Connection failed ";
+                    die("Connection failed: " . $conn->connect_error);
+                } 
+
+                $file = fopen($options["file"],"r");
+
+                while (($data = fgetcsv($file)) !== FALSE) {
+                    if (trim($data[2]) != 'email'){
+                        if (filter_var($data[2], FILTER_VALIDATE_EMAIL)){
+
+                            $name = trim(ucfirst(strtolower($data[0])));
+                            $surname = trim(ucfirst(strtolower($data[1])));
+                            $email = trim(strtolower($data[2]));
+
+                            $sql = "INSERT INTO users (name, surname, email)
+                            VALUES ('$name', '$surname', '$email')";
+                            
+                            if ($conn->query($sql) === TRUE) {
+                                $line = $line.date("d/m/Y H:i:s")." New record created successfully \n";
+                                echo date("d/m/Y H:i:s")." New record created successfully \n";
+                            } else {
+                                $line = $line.date("d/m/Y H:i:s")." Error: " . $sql . "\n" . $conn->error."\n";
+                                echo date("d/m/Y H:i:s")." Error: " . $sql . "\n" . $conn->error."\n";
+                            }
+
+                        }else{
+                            $line = $line.date("d/m/Y H:i:s")." Record not created, invalid email format: ".$data[2]."\n";
+                            echo date("d/m/Y H:i:s")." Record not created, invalid email format: ".$data[2]."\n";
+                        }
+                    }
+
+                }
+
+                fwrite($stdout, $line);
+
+                fclose($stdout);
+
+                fclose($file);
+
+                $conn->close();
                 break;
             case isset($options["create_table"]):
                 $stdouts = fopen('stdout.log', 'r');
@@ -44,12 +98,14 @@
                 $conn = new mysqli($hostname, $username, $password, $dbname);
                 if ($conn->connect_error) {
                     fwrite($stdout, $line.date("d/m/Y H:i:s")." Database Connection failed: ". $conn->connect_error."\n");
+                    echo date("d/m/Y H:i:s")." Database Connection failed ";
                     die("Database Connection failed: " . $conn->connect_error);
                 } 
                 $sql = "DROP TABLE IF EXISTS users";
                 
                 if ($conn->query($sql) === FALSE) {
                     fwrite($stdout, $line.date("d/m/Y H:i:s")." Error droping table: " . $conn->error."\n");
+                    echo date("d/m/Y H:i:s")." Error droping table: " . $conn->error;
                 }
                 
                 $sql = "CREATE TABLE users (
@@ -62,14 +118,53 @@
             
                 if ($conn->query($sql) === TRUE) {
                     fwrite($stdout, $line.date("d/m/Y H:i:s")." Table users created successfully\n");
+                    echo date("d/m/Y H:i:s")." Table users created successfully";
                 } else {
                     fwrite($stdout, $line.date("d/m/Y H:i:s")." Error creating table: " . $conn->error."\n");
+                    echo date("d/m/Y H:i:s")." Error creating table: " . $conn->error;
                 }
 
                 fclose($stdout);
                 $conn->close();
                 break;
             case isset($options["dry_run"]):
+                $stdouts = fopen('stdout.log', 'r');
+                $line = '';
+
+                while (!feof($stdouts)) {
+                    $line = $line.fgets($stdouts);
+                }
+
+                fclose($stdouts);
+
+                $stdout = fopen('stdout.log', 'w');
+
+                $file = fopen($options["dry_run"],"r");
+
+                while (($data = fgetcsv($file)) !== FALSE) {
+                    if (trim($data[2]) != 'email'){
+                        if (filter_var($data[2], FILTER_VALIDATE_EMAIL)){
+
+                            $name = trim(ucfirst(strtolower($data[0])));
+                            $surname = trim(ucfirst(strtolower($data[1])));
+                            $email = trim(strtolower($data[2]));
+
+                            $line = $line.date("d/m/Y H:i:s")." name: ".$name." ";
+                            $line = $line." surname: ".$surname." ";
+                            $line = $line." email: ".$email."\n";
+
+                        }else{
+                            $line = $line.date("d/m/Y H:i:s")." Invalid email format: ".$data[2]."\n";
+                            echo date("d/m/Y H:i:s")." Invalid email format: ".$data[2]."\n";
+                        }
+                    }
+
+                }
+
+                fwrite($stdout, $line);
+                fclose($stdout);
+                fclose($file);
+
                 break;
             case isset($options["help"]):
                 echo "
