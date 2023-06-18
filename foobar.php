dab6cd09796d66c7a65084c39055df427c18d0db
janmedinag <janmedinag@gmail.com>
2023-06-18 19:44:08 +1000

    janmedinag: foobar script.

--- foobar.php
+++ foobar.php
@@ -1,4 +1,23 @@
 <?php
 
+    $res = "1, 2, ";
+    for($i = 3; $i <= 99; $i++) {
+        if (fmod($i,3)==0){
+            if (fmod($i,5)==0){
+                $res = $res.'foobar, ';
+            }else{
+                $res = $res.'foo, ';
+            }
+        }else{
+            if (fmod($i,5)==0){
+                $res = $res.'bar, ';
+            }else{
+                $res = $res.$i.', ';
+            }
+        }
+    }
+    $res = $res.'bar.';
+
+    echo $res;
 
 ?>
